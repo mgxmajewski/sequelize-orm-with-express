3bcae23877e8a428de94ae34cd290140c03f808d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-28 02:32:40 +0100

    Add update handler

--- routes/articles.js
+++ routes/articles.js
@@ -36,19 +36,21 @@ router.post('/', asyncHandler(async (req, res) => {
 
 /* Edit article form. */
 router.get("/:id/edit", asyncHandler(async(req, res) => {
-  res.render("articles/edit", { article: {}, title: "Edit Article" });
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/edit", { article, title: article.title });
 }));
 
 /* GET individual article. */
 router.get("/:id", asyncHandler(async (req, res) => {
   const article = await Article.findByPk(req.params.id);
-
   res.render("articles/show", { article, title: article.title });
 }));
 
 /* Update an article. */
 router.post('/:id/edit', asyncHandler(async (req, res) => {
-  res.redirect("/articles/");
+  const article = await Article.findByPk(req.params.id);
+  await article.update(req.body);
+  res.redirect("/articles/" + article.id);
 }));
 
 /* Delete article form. */
