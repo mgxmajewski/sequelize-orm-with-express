ddd0d88eb5c6e7d318627a0ea385ed3b2ac450ba
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-28 02:28:09 +0100

    Revert "Add Update and Delete Entries handlers"
    This reverts commit cf5b1656

--- routes/articles.js
+++ routes/articles.js
@@ -36,9 +36,7 @@ router.post('/', asyncHandler(async (req, res) => {
 
 /* Edit article form. */
 router.get("/:id/edit", asyncHandler(async(req, res) => {
-  const article = await Article.findByPk(req.params.id);
-
-  res.render("articles/edit", { article, title: "Edit Article" });
+  res.render("articles/edit", { article: {}, title: "Edit Article" });
 }));
 
 /* GET individual article. */
@@ -50,9 +48,7 @@ router.get("/:id", asyncHandler(async (req, res) => {
 
 /* Update an article. */
 router.post('/:id/edit', asyncHandler(async (req, res) => {
-  const article = await Article.findByPk(req.params.id);
-  await article.update(req.body);
-  res.redirect("/articles/" + article);
+  res.redirect("/articles/");
 }));
 
 /* Delete article form. */
